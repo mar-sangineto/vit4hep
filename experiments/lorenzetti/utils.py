0940a466bd6aa5cb51b20e4d41f2f7526d75119b
Marina Sangineto Juca <msangine@lxplus903.cern.ch>
2026-06-05 16:23:16 +0200

    log shift and cleared eta and phi for test

--- experiments/lorenzetti/utils.py
+++ experiments/lorenzetti/utils.py
@@ -3,27 +3,34 @@ import numpy as np
 
 
 def load_data(data_file, n_layers=17):
+    # Layer energy is in MeV(1e6); truth energy is in GeV(1e9)
+    # Electrons simulation was done from 2GeV to 7 TeV
+    
     data={}
+    shift=5000 # 5GeV for layers to avoid 0 and negative values
     
     with h5py.File(data_file, "r") as full_file:
-        for i in range(n_layers):
-            layer_name = f"layer_{i}"
+        
+        for i in range(n_layers): #assumes that the first items are the layers
+            
+            layer_name = list(full_file.keys())[i]
             
             layer_data = full_file[layer_name][:]
             
-            data[layer_name] = np.log10(layer_data+1)
+            data[f"layer_{i}"] = np.log10(layer_data+shift) # log is to deal with a large range of values
             
         energy = full_file["energy"][:]
-        data["energy"] = np.log10(energy)
+        data["energy"] = np.log10(energy) # truth particle wont have negative energy
         
-        # to integrate C
-        if "eta" in full_file:
-            data["eta"] = full_file["eta"][:]
-        if "phi" in full_file:
-            data["phi"] = full_file["phi"][:] # Integrate as an output from an MLP
+        ### Next step : implement eta and phi. Initial tests with just energy so that it works as it is working in calogan
+#         # to integrate C
+#         if "eta" in full_file:
+#             data["eta"] = full_file["eta"][:]
+#         if "phi" in full_file:
+#             data["phi"] = full_file["phi"][:] # IDEA: Integrate as an output from an MLP
             
-        elif "truth_kinematics" in full_file:
-            data["eta"] = full_file["truth_kinematics"][:][:, 0]
-            data["phi"] = full_file["truth_kinematics"][:][:, 1]
+#         elif "truth_kinematics" in full_file:
+#             data["eta"] = full_file["truth_kinematics"][:][:, 0]
+#             data["phi"] = full_file["truth_kinematics"][:][:, 1]
     
     return data
